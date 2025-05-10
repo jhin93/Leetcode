0393ddfdc718b02bade819ceb1323669b53ffb6f
jhin93 <scene1993@gmail.com>
2025-05-11 00:39:22 +1000

    Read the problem correctly

---
+++ 1768.Merge_Strings_Alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ''
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1): result += word1[i]
+            if i < len(word2): result += word2[i]
+
+        return result
+
+solution = Solution()
+result = solution.mergeAlternately('1357', '246')                    
+
+
+                        
+
+        
