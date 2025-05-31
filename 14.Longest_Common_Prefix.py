770f5901ff9a7304841ed6d3f72045a0d27b9db3
jhin93 <scene1993@gmail.com>
2025-05-31 21:53:07 +1000

    saving commit

--- 14.Longest_Common_Prefix.py
+++ 14.Longest_Common_Prefix.py
@@ -1,2 +1,6 @@
-
-
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        output = ''
+        for i in range(len(strs) - 1):
+            print(strs[i + 1])
+        
